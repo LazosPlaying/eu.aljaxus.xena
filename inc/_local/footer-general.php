2240b1eb23cb7c9287b090f49978b7944cc10e6f
aljaxus <aljaxus.dev@gmail.com>
2018-09-15 15:49:37 +0200

    Footer(-s)

---
+++ inc/_local/footer-general.php
@@ -0,0 +1,38 @@
+<!-- START FOOTER -->
+<footer class="page-footer blue darken-1" id="footer" style="padding-top:0;">
+  	<div class="container">
+    	<div class="row">
+      		<div class="col s12 l6">
+        		<ul>
+          			<li class="togglePreloader">
+						<p class="grey-text text-lighten-4"></p>
+						<div class="switch">
+							<label class="grey-text text-lighten-4">
+									Toggle preloader - Off
+								<input type="checkbox">
+								<span class="lever"></span>
+									On
+							</label>
+						</div>
+					</li>
+        		</ul>
+      		</div>
+      		<div class="col s12 l3 offset-l3">
+        		<ul>
+          			<li><a class="grey-text text-lighten-3" href="/about">Read more about Xena</a></li>
+        		</ul>
+      		</div>
+    	</div>
+  	</div>
+  	<div class="footer-copyright">
+    	<div class="container">
+			© 2018 The Xena Project - aljaxus.eu
+    		<span class="right">
+				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" target="_blank" href="https://dev.aljaxus.eu"><i class="fas fa-code"></i> aljaxus</a>
+				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light terms-open" href="https://xena.aljaxus.eu/terms.html">Terms of usage</a>
+				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" href="/me" >User area</a>
+			</span>
+    	</div>
+  	</div>
+</footer>
+<!-- END FOOTER -->
---
+++ inc/_local/footer-site.php
@@ -0,0 +1,12 @@
+<!-- START FOOTER -->
+<footer class="page-footer blue darken-1" id="footer" style="padding-top:0;">
+  	<div class="footer-copyright" style="padding:2px 0!important;min-height:unset">
+    	<div class="container">
+			© 2018 The Xena Project - aljaxus.eu
+    		<span class="right">
+				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light terms-open" href="https://xena.aljaxus.eu/terms.html">Terms of usage</a>
+			</span>
+    	</div>
+  	</div>
+</footer>
+<!-- END FOOTER -->

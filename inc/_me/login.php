1f36bf82b653b61bef4b62220927a78e1ce8aa78
aljaxus <aljaxus.dev@gmail.com>
2018-09-15 15:49:56 +0200

    Login page

---
+++ inc/_me/login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- START META TAGS -->
+	<title>Me @ Xena</title>
+	<?php require_once __DIR__ . '/../_local/_head_meta.php'; ?>
+	<?php require_once __DIR__ . '/../_local/_head_includes.php'; ?>
+</head>
+<body>
+<div id="loader-wrapper"></div>
+<main>
+
+<?php require_once __DIR__ . '/../_local/navbar-sidebar.php'; ?>
+
+<!-- START CONTENT -->
+<div class="row">
+	<div class="col s12 m8 l4 offset-s0 offset-m2 offset-l4">
+		<div class="card blue lighten-3" style="margin-top: 10vh;">
+			<div class="card-tabs">
+				<ul class="tabs tabs-fixed-width">
+					<li class="tab"><a class="active" href="#google_signin">Sign in with google</a></li>
+				</ul>
+			</div>
+			<div class="card-content grey lighten-4">
+				<div id="google_signin">
+					<div class="row">
+						<div class="col s12 m6 l6 offset-s0 offset-m3 offset-l3">
+							<div
+								class="g-signin2 center-align"
+								data-onfailure="googleOauth_onSignInError"
+								data-onsuccess="googleOauth_onSignInSuccess"
+								data-longtitle="true"
+								data-theme="dark"
+								data-prompt="select_account"
+								data-immediate="false"
+							></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END CONTENT -->
+
+</main>
+<?php require_once __DIR__ . '/../_local/footer-general.php'; ?>
+<?php require_once __DIR__ . '/../_local/modal-terms.html'; ?>
+
+<!--
+<div
+	class="g-signin2"
+	data-onfailure="googleOauth_onSignInError"
+	data-onsuccess="googleOauth_onSignInSuccess"
+	data-theme="dark"
+></div>
+<a href="javascript:void(0)" onclick="googleOauth_onSignOut();">Sign out</a>
+ -->
+
+</body>
+</html>
+<!-- START CSS FILES -->
+<style media="all">
+div.abcRioButton.abcRioButtonBlue{
+	width: 100%!important;
+}
+</style>
+<!-- END CSS FILES -->
+
+<!-- START SECONDARY SCRIPTS -->
+<script>
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.tabs').tabs();
+});
+</script>
+<!-- END SECONDARY SCRIPTS -->

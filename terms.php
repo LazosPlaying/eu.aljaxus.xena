c7352917ab15d1f715e3875bc23a0e7ebac147cc
aljaxus <aljaxus.dev@gmail.com>
2018-09-15 15:52:06 +0200

    Terms Of Usage and Privacy Policy

---
+++ terms.php
@@ -0,0 +1,26 @@
+<h5>1 - Terms of usage</h5>
+<p>1.1 - By accessing this website ('The Xena project', 'The Xena', 'Xena', 'The project', 'Xena project', 'we', 'us', 'our', 'ours', 'this service', 'the service', 'xena.aljaxus.eu'), you are agreeing to be bound by these website’s Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not or have failed to comply with any of our terms and conditions you are prohibited from using or accessing this site and/or the entirety of our services.</p>
+<p>1.2 - We reserve the right to store any data that you might provide at any point of using our service</p>
+<p>1.3 - The Xena project may modify these terms or any additional terms in order to reflect changes to the law or to any service provided by The Xena project. You should look at the terms regularly.</p>
+<h5>2 - Disclaimer</h5>
+<p>2.1 - All the material and data provided on Xena project service is provided "as is". The Xena project makes no warranties for keeping all the data up to date and accurate.</p>
+<h5>3 - Privacy policy</h5>
+<p>3.1 - Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.</p>
+<p>3.2 - Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</p>
+<p>3.3 - All personal data is being and will be collected and used solely with the objective of providing best user experience.</p>
+<p>3.4 - All personal data that we store is kept in secured databases and is not nor will be redistributed to any third party services.</p>
+<p>3.5 - List of all data collected from external sources and information why we collect it and how we use it</p>
+<p>3.5.1 - Google oAuth service</p>
+<p>3.5.1.1 - Email address -> Used for identifying users. Example: signing into the application system</p>
+<p>3.5.1.2 - Given name -> Used for better, more personalised, user experience</p>
+<p>3.5.1.3 - Family name -> Used for better, more personalised, user experience</p>
+<p>3.5.1.4 - Full name -> Used for better, more personalised, user experience. A sum of Given and Family name(-s). Example: My panel (desktop), Side navigation (mobile, tablet)</p>
+<p>3.5.1.5 - Profile picture URL -> Used for better, more personalised, user experience. Example: My panel (desktop), Side navigation (mobile, tablet)</p>
+<p>3.5.1.6 - Locale -> Used for better, more personalised, user experience.</p>
+<h5>4 - Cookies</h5>
+<p>4.1 - For better user experience we are using cookies. By using this site, you are unconditionally agreeing to use of online cookies and storing cookies and their data in your browser and/or browser's data folder.</p>
+<p>4.2 - The Xena project is using <a href="https://analytics.google.com" target="_blank">Google Analytics</a> for collecting data that helps us improve our platform. By agreeing to these terms you are also agreeing to their <a href="https://www.google.com/analytics/terms/us.html" target="_blank">Terms Of Service</a>.</p>
+<h5>5 - User agreement</h5>
+<p>5.1 - You understand and agree that your use of this website and any services or content provided is made available and provided to you at your own risk. It is provided to you as-is and we expressly disclaim all warranties of any kind, expressed or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. You understand and agree that neither us nor any participant in the service provides professional advice of any kind and thus use of such advice or any other information is solely at your own risk and without our liability of any kind. Some jurisdictions may not allow disclaimers of implied warranties and the above disclaimer may not apply to you only as it relates to implied warranties.</p>
+<p>5.2 - You expressly understand and agree that we shall not be liable for any direct, indirect, special, incidental, consequential or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data or other intangible loss (even if we have been advised of the possibility of such damages), resulting from or arising out of (I) The use of or the inability to use the service, (II) The cost to obtain substitute goods and/or services resulting from any transaction entered into or through the service, (III) Unauthorized access to or alteration of your data transmissions, (IV) Statements or conduct of any third party on the service, or (V) any other matter relating to the service. Note that by accepting the terms in the document, you are also waiving your right, to take any action, legal or otherwise, against anyone or anything related to the administrators, members or any users of this service. You furthermore acknowledge that if any part of this document is found to be invalid or unenforceable in any way, any parts not found to be invalid or unenforceable will be considered fully valid and binding.</p>
+<p>5.3 - In case you do not agree with terms of usage you are not allowed to use or interract with any service provided or associated with The Xena project.</p>

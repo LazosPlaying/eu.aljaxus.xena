1c2b0c96f8899c18409204ca5eaf2430e8fd4f19
aljaxus <aljaxus.dev@gmail.com>
2018-09-15 17:43:39 +0200

    Updated GitHub prepo to new URL

--- inc/_local/footer-general.php
+++ inc/_local/footer-general.php
@@ -29,6 +29,7 @@
 			© 2018 The Xena Project - aljaxus.eu
     		<span class="right">
 				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" target="_blank" href="https://dev.aljaxus.eu"><i class="fas fa-code"></i> aljaxus</a>
+				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" target="_blank" href="https://github.com/aljaxus/eu.aljaxus.xena/"><i class="fab fa-github"></i> GitHub</a>
 				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light terms-open" href="https://xena.aljaxus.eu/terms.html">Terms of usage</a>
 				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" href="/me" >User area</a>
 			</span>

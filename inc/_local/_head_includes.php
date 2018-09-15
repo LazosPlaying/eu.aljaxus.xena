47cc76c803257215a99c46504d60189003b5ee08
aljaxus <aljaxus.dev@gmail.com>
2018-09-15 15:49:22 +0200

    <head> components

---
+++ inc/_local/_head_includes.php
@@ -0,0 +1,25 @@
+<!-- START SCRIPTS -->
+<script src="https://static.aljaxus.eu/lib/jquery/jquery-3.3.1/jquery.min.js" charset="utf-8"></script>
+<script src="https://static.aljaxus.eu/lib/js-cookie/js-cookie-v2.2.0.js" charset="utf-8"></script>
+<script src="https://xena.aljaxus.eu/src/js/preloader.js" charset="utf-8"></script>
+<script src="https://static.aljaxus.eu/lib/materializecss/materialize-v1.0.0-rc.2/js/materialize.min.js" charset="utf-8"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="https://static.aljaxus.eu/lib/jquery-prettydate/prettydate.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" crossorigin="anonymous"></script>
+<!-- END SCRIPTS -->
+<!-- START STYLES -->
+<link rel="stylesheet" href="https://static.aljaxus.eu/lib/materializecss/materialize-v1.0.0-rc.2/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Sunflower:300|Source+Sans+Pro|Tillana" rel="stylesheet">
+<link rel="stylesheet" href="/src/css/preloader.css">
+<link rel="stylesheet" href="/src/css/master.css">
+<link rel="stylesheet" href="/src/css/toasts.css">
+
+<style media="all">body{opacity:0;transition:opacity 0.3s;}body:not(.loaded){overflow:hidden;}</style>
+<!-- END STYLES -->
+
+<!-- START GOOGLE OAUTH -->
+<meta name="google-signin-scope" content="profile email">
+<meta name="google-signin-client_id" content="126687455857-9p9psccqqltlbtipohq6m93fm7fvau4p.apps.googleusercontent.com">
+<script src="/src/js/google_oauth.js" charset="utf-8"></script>
+<!-- END GOOGLE OAUTH -->
---
+++ inc/_local/_head_meta.php
@@ -0,0 +1,39 @@
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<base href="https://xena.aljaxus.eu/" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="Simple tools for students - teachers - education">
+<meta name="keywords" content="zoroja, sites, free, scng, solski center nova gorica, šolski center nova gorica, nova gorica, tšc, tsc, tscng, tšcng sol">
+
+<link rel="apple-touch-icon" href="https://xena.aljaxus.eu/src/icon_logo_16px.png" />
+<link rel="apple-touch-icon" sizes="32x32" href="https://xena.aljaxus.eu/src/icon_logo_32px.png" />
+<link rel="apple-touch-icon" sizes="64x64" href="https://xena.aljaxus.eu/src/icon_logo_64px.png" />
+<link rel="apple-touch-icon" sizes="256x256" href="https://xena.aljaxus.eu/src/icon_logo_256px.png" />
+<link rel="apple-touch-icon" sizes="512x512" href="https://xena.aljaxus.eu/src/icon_logo_512px.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="https://xena.aljaxus.eu/src/icon_logo_16px.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="https://xena.aljaxus.eu/src/icon_logo_32px.png" />
+<link rel="icon" type="image/png" sizes="64x64" href="https://xena.aljaxus.eu/src/icon_logo_64px.png" />
+<link rel="icon" type="image/png" sizes="256x256" href="https://xena.aljaxus.eu/src/icon_logo_256px.png" />
+<link rel="icon" type="image/png" sizes="512x512" href="https://xena.aljaxus.eu/src/icon_logo_512px.png" />
+<link rel="manifest" href="https://xena.aljaxus.eu/src/icon_logo_manifest.json" />
+
+<!-- END META TAGS -->
+<!-- START SHARING TAGS -->
+<meta property="og:site_name" content="Xena">
+<meta property="og:url" content="https://xena.aljaxus.eu/">
+<meta name="twitter:url" content="https://xena.aljaxus.eu/">
+<meta property="og:type" content="website">
+<meta name="author" content="aljaxus">
+<meta name="twitter:card" content="summary">
+<meta property="og:title" content="The Xena project">
+<meta property="og:image" content="https://xena.aljaxus.eu/src/icon_logo_512px.png">
+<meta property="og:description" content="Xena is a project started by aljaxus. It provides
+- Simple authentication: Just login using Google account
+- Many useful tools: Simple webpage maker, file uploader and more
+- Content in multiple languages: It's not limited by any means
+- Free tools - for students - for teachers - for education">
+
+<meta name="twitter:title" content="The Xena project">
+<meta name="twitter:description" content="Simple tools for students - teachers - education">
+<meta name="twitter:image" content="https://xena.aljaxus.eu/src/icon_logo_512px.png">
+<!-- END SHARING TAGS -->

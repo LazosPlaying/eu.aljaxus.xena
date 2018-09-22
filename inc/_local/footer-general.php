7f05845ecf2dd06d741a6c6616314a5c7d7a34dc
aljaxus <aljaxus.dev@gmail.com>
2018-09-22 22:41:06 +0200

    Moved TOS link to other section of footer

--- inc/_local/footer-general.php
+++ inc/_local/footer-general.php
@@ -19,8 +19,9 @@
       		</div>
       		<div class="col s12 l3 offset-l3">
         		<ul>
-          			<li><a class="grey-text text-lighten-3" href="/about">Read more about Xena</a></li>
+          			<li><a class="grey-text text-lighten-3" href="/about">About</a></li>
           			<li><a class="grey-text text-lighten-3" href="/me">My dashboard</a></li>
+          			<li><a class="grey-text text-lighten-3 terms-open" href="/terms.php">Terms of usage</a></li>
         		</ul>
       		</div>
     	</div>
@@ -31,7 +32,6 @@
     		<span class="right">
 				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" target="_blank" href="https://dev.aljaxus.eu"><i class="fas fa-code"></i> aljaxus</a>
 				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light" target="_blank" href="https://github.com/aljaxus/eu.aljaxus.xena/"><i class="fab fa-github"></i> GitHub</a>
-				<a class="grey-text text-lighten-4 btn btn-flat btn-small waves-effect waves-light terms-open" href="https://xena.aljaxus.eu/terms.html">Terms of usage</a>
 			</span>
     	</div>
   	</div>

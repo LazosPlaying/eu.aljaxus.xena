c3b2dd3d3b24d1e50f2d1372f2293fb901d79fa4
aljaxus <aljaxus.dev@gmail.com>
2018-09-22 22:42:14 +0200

    Added option for sub-section of /me/ section

--- me.php
+++ me.php
@@ -2,7 +2,16 @@
 require_once __DIR__ . '/inc/_util/firstload.php';
 
 if (!empty($_SESSION['u_isloged']) && $_SESSION['u_isloged'] === true ){
-	require_once __DIR__ . '/inc/_me/dashboard.php';
+	if (!empty($_GET['par1'])){
+		$path = __DIR__ . '/inc/_me/'.$_GET['par1'].'.php';
+		if (file_exists($path)){
+			require_once $path;
+		} else {
+			require_once __DIR__ . '/error_404.php';
+		}
+	} else {
+		require_once __DIR__ . '/inc/_me/dashboard.php';
+	}
 } else {
 	require_once __DIR__ . '/inc/_me/login.php';
 }

96b93e194c3a675f7f9a76a833df3ba97d84c2f0
aljaxus <aljaxus.dev@gmail.com>
2018-09-24 22:13:45 +0200

    Slowly adapting to PSR standard
    https://www.php-fig.org/psr/psr-1/

--- inc/_util/database.php
+++ inc/_util/database.php
@@ -1,11 +1,14 @@
 <?php
 require_once __DIR__ . '/settings.nogit.php';
 
-class dbInit extends mysqlAuth {
+class dbInit extends mysqlAuth
+{
 
-	public function mysqli(){
+	public function mysqli()
+	{
 		$conn = mysqli_connect($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
-		if (mysqli_connect_errno()){
+		if (mysqli_connect_errno())
+		{
 		    die("Failed to connect to database: ".mysqli_connect_error()."<br/> Inform the administrator");
 		}
 		mysqli_query($conn, "SET NAMES utf8");
@@ -13,9 +16,11 @@ class dbInit extends mysqlAuth {
 		return $conn;
 	}
 
-	public function oopmysqli(){
+	public function oopmysqli()
+	{
 		$conn = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
-		if (mysqli_connect_errno()){
+		if (mysqli_connect_errno())
+		{
 		    die("Failed to connect to database: ".mysqli_connect_error()."<br/> Inform the administrator");
 		}
 		$conn->query("SET NAMES utf8");
@@ -23,20 +28,21 @@ class dbInit extends mysqlAuth {
 		return $conn;
 	}
 
-	public function pdo(){
+	public function pdo()
+	{
 		$dsn = "mysql:dbname=$this->dbname;host=$this->dbhost;charset=utf8";
-		try {
+		try
+		{
 		    $pdo = new PDO($dsn, $this->dbuser, $this->dbpass);
 		    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
-		} catch (PDOException $e) {
+		} catch (PDOException $e)
+		{
 		    die("Failed to connect to the database: ".$e."<br/> Inform an administrator");
 		    exit();
 		}
 	}
 }
-class dbManip extends dbInit {
-	public function select($datArr){
 
 	}
 }
